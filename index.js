11d5b988deb31ed0dcb4275defb1fe13775e9fe2
Dan Buda <danbuda@gmail.com>
2020-01-30 17:31:59 -0600

    rollDie func cleaned up, added favicons

--- index.js
+++ index.js
@@ -1,35 +1,48 @@
-const four = document.getElementById('four');
-const six = document.getElementById('six');
-const eight = document.getElementById('eight');
-const ten = document.getElementById('ten');
-const twelve = document.getElementById('twelve');
-const twenty = document.getElementById('twenty');
-const hundred = document.getElementById('hundred');
+// const four = document.getElementById('four');
+// const six = document.getElementById('six');
+// const eight = document.getElementById('eight');
+// const ten = document.getElementById('ten');
+// const twelve = document.getElementById('twelve');
+// const twenty = document.getElementById('twenty');
+// const hundred = document.getElementById('hundred');
 const roll = document.getElementById('roll');
 
-four.addEventListener('click', () => {
-  rollDie(4);
-});
-six.addEventListener('click', () => {
-  rollDie(6);
-});
-eight.addEventListener('click', () => {
-  rollDie(8);
-});
-ten.addEventListener('click', () => {
-  rollDie(10);
-});
-twelve.addEventListener('click', () => {
-  rollDie(12);
-});
-twenty.addEventListener('click', () => {
-  rollDie(20);
-});
-hundred.addEventListener('click', () => {
-  rollDie(100);
+const dice = document.querySelectorAll('.die');
+dice.forEach(die => {
+  die.addEventListener('click', () => {
+    rollDie(die.dataset.number)}
+    )
 });
 
+// Probably need to disable all dice/buttons while this is running then enable once roll is complete
+// function rollDie(x) { 
+//     window.setInterval(() => {
+//       const number = Math.ceil(Math.random() * x);
+//       roll.innerHTML = number;}, 500)
+   
+// }
+
+// function rollDie(x) {
+//   for (i = 0; i < 7; i++) {
+//     window.setTimeout(() => {
+//       let number = Math.ceil(Math.random() * x);
+//       roll.innerHTML = number;
+//     }, 500)
+//   }
+// }
+
 function rollDie(x) {
-  const number = Math.ceil(Math.random() * x);
-  roll.innerHTML = number;
+  let counter = 0;
+  let interval = setInterval(() => {
+    roll.style.color = 'lightblue';
+    counter+= 1;
+    if(counter === 6) {
+      let number = Math.ceil(Math.random() * x);
+      roll.innerHtml = number;
+      roll.style.color = 'black';
+      clearInterval(interval);
+    }
+    let number = Math.ceil(Math.random() * x);
+    roll.innerHTML = number;
+  }, 200);
 }
