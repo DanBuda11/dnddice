98f9e7de8ad9607ab949e39e6577ff05ea399324
Dan Buda <danbuda@gmail.com>
2020-07-03 21:34:14 -0500

    updated linting & # of die now tied to each die

--- src/js/index.js
+++ src/js/index.js
@@ -1,33 +1,38 @@
 const plusBtns = document.querySelectorAll('.die-plus');
 const rollBtns = document.querySelectorAll('.die-roll');
 const eachRoll = document.getElementById('each-roll');
-const totalRoll = document.getElementById('total-roll')
-let dieCounter = 1
+const totalRoll = document.getElementById('total-roll');
+// dieCounter needs to be connected to the individual die or it will roll that number of dice for any die that is rolled - if dieCounter is 3 because you hit plus on d4 twice, it will still roll 3 dice if you roll a d20 but its counter only shows a 1 - this should be working properly now
+// let dieCounter = 1;
 
-rollBtns.forEach(btn => {
+rollBtns.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     rollDice(e.target.parentNode.dataset.number);
-    btn.innerHTML = '1'
-  })
-})
+    btn.innerHTML = '1';
+  });
+});
 
-plusBtns.forEach(btn => {
+plusBtns.forEach((btn) => {
   btn.addEventListener('click', () => {
-    dieCounter++
-    btn.nextElementSibling.innerHTML = dieCounter.toString()
-  })
-})
+    btn.nextElementSibling.dataset.counter = Number(btn.nextElementSibling.dataset.counter) + 1;
+    console.log(btn.nextElementSibling.dataset.counter);
+    // dieCounter++;
+    btn.nextElementSibling.innerHTML = btn.nextElementSibling.dataset.counter.toString();
+  });
+});
 
 function rollDice(x) {
   // console.log(`This die has ${x} sides.`);
-  let rolls = []
-  let sum = 0
-  for (i = 1; i <= dieCounter; i++) {
-    let roll = Math.ceil(Math.random() * parseInt(x))
-    rolls.push(`${roll} +`)
-    sum += roll
+  const thisDie = document.querySelector(`.die-roll[data-number="${x}"]`);
+  console.log(thisDie);
+  const rolls = [];
+  let sum = 0;
+  for (i = 1; i <= thisDie.dataset.counter; i++) {
+    const roll = Math.ceil(Math.random() * parseInt(x));
+    rolls.push(`${roll} +`);
+    sum += roll;
   }
-  eachRoll.innerHTML = rolls.join(' ')
-  totalRoll.innerHTML = sum
-  dieCounter = 1
+  eachRoll.innerHTML = rolls.join(' ');
+  totalRoll.innerHTML = sum;
+  thisDie.dataset.counter = 1;
 }
--- src/js/old-index.js
+++ src/js/old-index.js
@@ -8,29 +8,29 @@
 const roll = document.getElementById('roll');
 
 const dice = document.querySelectorAll('.die');
-dice.forEach(die => {
+dice.forEach((die) => {
   die.addEventListener('click', () => {
-    rollDie(die.dataset.number)}
-    )
+    rollDie(die.dataset.number);
+  });
 });
 
-let counter = 0;
+const counter = 0;
 
 // Roll a single die
 function rollDie(x) {
   document.querySelector(`.die[data-number="${x}"`).classList.add('rotating');
-  dice.forEach(die => {
+  dice.forEach((die) => {
     die.setAttribute('disabled', 'disabled');
-  })
+  });
   setTimeout(() => {
     document.querySelector(`.die[data-number="${x}"`).classList.remove('rotating');
   }, 600);
   let number;
   let counter = 0;
-  let interval = setInterval(() => {
+  const interval = setInterval(() => {
     roll.style.color = 'lightblue';
-    counter+= 1;
-    if(counter === 6) {
+    counter += 1;
+    if (counter === 6) {
       number = Math.ceil(Math.random() * parseInt(x));
       // Set number = 20 when testing nat20 animation
       // number = 20;
@@ -42,9 +42,9 @@ function rollDie(x) {
           roll.classList.remove('nat-twenty');
         }, 1000);
       }
-      dice.forEach(die => {
+      dice.forEach((die) => {
         die.removeAttribute('disabled');
-      })
+      });
       clearInterval(interval);
     } else {
       number = Math.ceil(Math.random() * parseInt(x));
@@ -54,11 +54,8 @@ function rollDie(x) {
 }
 
 // Roll multiple of the same die
-function rollDice() {
-
-}
+function rollDice() {}
 
 // Tap/click the ROLL button to start rolling
-
 
 // Tap/click the CLEAR button to remove any already tapped-on die
