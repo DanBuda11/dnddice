d6f1d7845159194d42d5130b3782598469266487
Dan Buda <danbuda@gmail.com>
2023-11-25 17:15:00 -0500

    update per gulp-framework, sass -> css nesting

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,37 @@
 // ************************* Imports *************************
 
-const { src, dest, series, parallel, watch } = require('gulp');
+import gulp from 'gulp';
 // BrowserSync for dev server and hot reloading
-const bs = require('browser-sync').create();
-const sass = require('gulp-sass')(require('sass'));
-sass.compiler = require('sass');
-// Minimize HTML
-const htmlmin = require('gulp-htmlmin');
-// Minimize & optimize CSS
-const cleanCSS = require('gulp-clean-css');
-// Remove unused/dead CSS
-const purifyCSS = require('gulp-purifycss');
-// PostCSS with autoprefixer
-const postCSS = require('gulp-postcss');
-// Babel for Gulp
-const babel = require('gulp-babel');
-// Minimize JS
-const uglify = require('gulp-uglify');
-// Minify images
-const imagemin = require('gulp-imagemin');
-// Show sizes of files in the terminal
-const size = require('gulp-size');
-// Remove comments from files for production
-const strip = require('gulp-strip-comments');
+import browserSync from 'browser-sync';
 // Used to wipe contents of dist when running build task
-const del = require('del');
+import { deleteAsync } from 'del';
+// Minimize HTML
+import htmlmin from 'gulp-htmlmin';
+// Minimize & optimize CSS
+import cleanCSS from 'gulp-clean-css';
+// Remove unused/dead CSS
+import purgecss from 'gulp-purgecss';
+// PostCSS (using autoprefixer plugin)
+import postCSS from 'gulp-postcss';
+// PostCSS Nesting as a postCSS plugin
+import postcssNesting from 'postcss-nesting';
+// Autoprefixer as a postCSS plugin
+import autoprefixer from 'autoprefixer';
+// Babel for Gulp
+import babel from 'gulp-babel';
+// Minimize JS
+import uglify from 'gulp-uglify';
+// Minify images
+import imagemin from 'gulp-imagemin';
+// Show sizes of files in the terminal
+import size from 'gulp-size';
+// Remove comments from files for production
+import strip from 'gulp-strip-comments';
+
+// Import named methods from gulp
+const { src, dest, series, parallel, watch } = gulp;
+// Rename browserSync to shorten (personal preference)
+const bs = browserSync;
 
 // ************************* Folder Paths *************************
 
@@ -32,51 +39,16 @@ const paths = {
   input: 'src',
   output: 'dist',
   devHTML: 'src/*.html',
-  devCSS: 'src/css',
-  devSCSS: 'src/scss/*.scss',
+  devCSS: 'src/css/*.css',
   devJS: 'src/js/*.js',
   devImages: 'src/images/*.{png,gif,jpg,jpeg,svg}',
   devFavicons: 'src/*.{ico,png,xml,svg,webmanifest}',
   prodCSS: 'dist/css',
   prodJS: 'dist/js',
   prodImages: 'dist/images',
-  normalize: 'src/css/normalize.css',
 };
 
-// ************************* Development Tasks *************************
-
-// Compile Sass to CSS in development
-function serveSass() {
-  return src(paths.devSCSS)
-    .pipe(sass())
-    .pipe(dest(paths.devCSS))
-    .pipe(bs.stream());
-}
-
-// Task to run the BrowserSync server
-function browserSync() {
-  // Run serveSass when starting the dev server to make sure the SCSS & dev CSS are the same
-  serveSass();
-
-  bs.init({
-    // Dev server will run at localhost:8080
-    port: 8080,
-    server: {
-      baseDir: paths.input,
-    },
-  });
-
-  watch(paths.devHTML).on('change', bs.reload);
-  watch(paths.devSCSS, serveSass);
-  watch(paths.devJS).on('change', bs.reload);
-}
-
 // ************************* Production Tasks *************************
-
-// Wipe contents of dist folder
-function clean() {
-  return del([`${paths.output}/**`, `!${paths.output}`]);
-}
 
 // Minimize HTML files
 function buildHTML() {
@@ -93,23 +65,20 @@ function buildFavicon() {
 }
 
 // Minimize CSS files and add prefixes if needed
-function buildCSS() {
-  return src(paths.devSCSS)
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(purifyCSS([paths.devHTML, paths.devJS]))
-    .pipe(cleanCSS())
-    .pipe(postCSS())
-    .pipe(size({ showFiles: true }))
-    .pipe(dest(paths.prodCSS));
-}
-
-// Move normalize.css from src/css to dist/css
-function buildNormalize() {
-  return src(paths.normalize)
-    .pipe(cleanCSS())
-    .pipe(size({ showFiles: true }))
-    .pipe(dest(paths.prodCSS));
+export function buildCSS() {
+  return (
+    src(paths.devCSS)
+      .pipe(postCSS([postcssNesting(), autoprefixer()]))
+      .pipe(
+        purgecss({
+          content: [paths.devHTML, paths.devJS],
+        }),
+      )
+      // .pipe(purifyCSS([paths.devHTML, paths.devJS]))
+      .pipe(cleanCSS())
+      .pipe(size({ showFiles: true }))
+      .pipe(dest(paths.prodCSS))
+  );
 }
 
 // Minimize JavaScript files
@@ -118,7 +87,7 @@ function buildJS() {
     .pipe(
       babel({
         presets: ['@babel/env'],
-      })
+      }),
     )
     .pipe(uglify())
     .pipe(size({ showFiles: true }))
@@ -133,21 +102,30 @@ function buildImages() {
     .pipe(dest(paths.prodImages));
 }
 
-// ************************* Exported Tasks *************************
+// ************************* Exported Tasks to Run on Command Line *************************
 
-// Run gulp serve in the terminal to start development mode
-exports.serve = browserSync;
-// Run gulp clean to empty dist folder
-exports.clean = clean;
-// Run gulp build to run production build
-exports.build = series(
+// BrowserSync dev server
+export function serve() {
+  bs.init({
+    // Dev server will run at localhost:8080
+    port: 8080,
+    server: {
+      baseDir: paths.input,
+    },
+  });
+
+  watch(paths.devHTML).on('change', bs.reload);
+  watch(paths.devCSS).on('change', bs.reload);
+  watch(paths.devJS).on('change', bs.reload);
+}
+
+// Wipe contents of dist folder
+export function clean() {
+  return deleteAsync([`${paths.output}/**`, `!${paths.output}`]);
+}
+
+// Run production build
+export const build = series(
   clean,
-  parallel(
-    buildHTML,
-    buildFavicon,
-    buildCSS,
-    buildNormalize,
-    buildJS,
-    buildImages
-  )
+  parallel(buildHTML, buildFavicon, buildCSS, buildJS, buildImages),
 );
